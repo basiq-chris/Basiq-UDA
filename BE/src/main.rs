603601ae1ab775e5f959d16dc1bf0bacc3a2e532
basiq-chris <chris.zappia@basiq.io>
2023-12-19 10:18:42 +1100

    BE: adding better logging for the main application

--- BE/src/main.rs
+++ BE/src/main.rs
@@ -2,9 +2,10 @@
 use BSAPI::{requestHandler::send_request, Token};
 use actix_web::{Responder, HttpServer, App, HttpResponseBuilder, web};
 use qstring::QString;
-use reqwest::{StatusCode, ResponseBuilderExt, RequestBuilder, header::ACCEPT, Client, Method};
+use reqwest::{StatusCode, Client, Method};
 use Basiq_API as BSAPI;
 use std::sync::Mutex;
+use Logger;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -32,29 +33,36 @@ struct ServerToken {
 
 #[actix_web::get("/token/{user_id}")]
 async fn get_client_token(request_body: web::Path<String>, server_token: web::Data<ServerToken>) -> impl Responder {
-    println!("INFO: GET request made to /token");
+    Logger::print_info("GET request made to /token");
     let user_id = request_body.into_inner();
-    println!("DEBUG: userID used: {}", user_id);
-    HttpResponseBuilder::new(StatusCode::CREATED)
-    .append_header(("Access-Control-Allow-Origin", "*"))
-    .append_header(("Access-Control-Allow-Methods", "GET,POST,DELETE"))
-    .append_header(("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"))
-    .body(BSAPI::requestHandler::send_request(reqwest::Client::new(), BSAPI::RequestType::Token(BSAPI::KeyType::CLIENT_ACCESS), reqwest::Method::POST, None, None).await.stringify())
-}
-
-#[actix_web::post("/createuser")]
-async fn create_user(response_body: String, server_token: actix_web::web::Data<ServerToken>) -> impl Responder {
-    let query = QString::from(response_body.as_str());
-    println!("DEBUG: Body Content: {:?}", query);
-    println!("DEBUG: Checking token health");
+    Logger::print_debug("userID used: ".to_owned() + user_id.as_str());
     let mut token = server_token.token.lock().unwrap();
     if token.has_expired() {
-        println!("INFO: Token Expired");
+        Logger::print_info("Token Expired");
         *token = get_server_token().await;
     }
     let tkn = token.clone();
     drop(token);
-    println!("INFO: POST request made to /createuser");
+    HttpResponseBuilder::new(StatusCode::CREATED)
+    .append_header(("Access-Control-Allow-Origin", "*"))
+    .append_header(("Access-Control-Allow-Methods", "GET,POST,DELETE"))
+    .append_header(("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"))
+    .body(BSAPI::requestHandler::send_request(reqwest::Client::new(), BSAPI::RequestType::Token(BSAPI::KeyType::CLIENT_ACCESS), reqwest::Method::POST, Some(tkn), Some(user_id)).await.stringify())
+}
+
+#[actix_web::post("/createuser")]
+async fn create_user(response_body: String, server_token: actix_web::web::Data<ServerToken>) -> impl Responder {
+    Logger::print_info("POST request made to /createuser");
+    let query = QString::from(response_body.as_str());
+    Logger::print_debug(query.clone());
+    Logger::print_debug("Checking Token health".to_string());
+    let mut token = server_token.token.lock().unwrap();
+    if token.has_expired() {
+        Logger::print_info("Token Expired");
+        *token = get_server_token().await;
+    }
+    let tkn = token.clone();
+    drop(token);
     HttpResponseBuilder::new(StatusCode::CREATED)
     .append_header(("Access-Control-Allow-Origin", "*"))
     .append_header(("Access-Control-Allow-Methods", "GET,POST,DELETE"))
@@ -65,16 +73,16 @@ async fn create_user(response_body: String, server_token: actix_web::web::Data<S
 #[actix_web::post("/createauthlink")]
 async fn create_auth_link(response_body: String, server_token: actix_web::web::Data<ServerToken>) -> impl Responder {
     let query = QString::from(response_body.as_str());
-    println!("DEBUG: Body Content: {:?}", query);
-    println!("DEBUG: Checking token health");
+    Logger::print_debug(query.clone());
+    Logger::print_debug("Checking token health".to_string());
     let mut token = server_token.token.lock().unwrap();
     if token.has_expired() {
-        println!("INFO: Token Expired");
+        Logger::print_info("Token Expired".to_string());
         *token = get_server_token().await;
     }
     let tkn = token.clone();
     drop(token);
-    println!("INFO: POST request made to /createauthlink");
+    Logger::print_info("POST request made to /createauthlink");
     HttpResponseBuilder::new(StatusCode::CREATED)
     .append_header(("Access-Control-Allow-Origin", "*"))
     .append_header(("Access-Control-Allow-Methods", "GET,POST,DELETE"))
@@ -85,16 +93,16 @@ async fn create_auth_link(response_body: String, server_token: actix_web::web::D
 #[actix_web::get("/getjob/{job_id}")]
 async fn get_job(url_params: web::Path<String>, server_token: actix_web::web::Data<ServerToken>) -> impl Responder {
     let query = url_params.into_inner();
-    println!("DEBUG: Body Content: {:?}", query);
-    println!("DEBUG: Checking token health");
+    Logger::print_debug(query.clone());
+    Logger::print_debug("Checking token health".to_string());
     let mut token = server_token.token.lock().unwrap();
     if token.has_expired() {
-        println!("INFO: Token Expired");
+        Logger::print_info("Token expired");
         *token = get_server_token().await;
     }
     let tkn = token.clone();
     drop(token);
-    println!("INFO: GET request made to /getjob");
+    Logger::print_info("GET request made to /getjob");
     HttpResponseBuilder::new(StatusCode::OK)
     .append_header(("Access-Control-Allow-Origin", "*"))
     .append_header(("Access-Control-Allow-Methods", "GET,POST,DELETE"))
